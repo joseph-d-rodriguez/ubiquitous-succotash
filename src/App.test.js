97bb8cd7fdd3875214196148211c126750b1e7cc
Joseph Rodriguez <joe_rodriguez2@apple.com>
2019-05-22 13:48:21 -0700

    More messing around with unit test.

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createOwGtStore } from './gt-boot';
+import assert from 'assert';
 
 it('renders without crashing', () => {
   const store = createOwGtStore();
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+
+  assert(1 === 1);
+
+  let appDom = div.firstElementChild;
+  assert(!!appDom);
+  assert(!!appDom.className);
+  assert(appDom.className.indexOf('App') !== -1);
+  assert(appDom.children.length === 2); // <header> and <div class="changes">
+
+
   ReactDOM.unmountComponentAtNode(div);
 });
